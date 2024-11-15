8a9d5c05edf1c348987969c0b8e81bb991a04365
amkoroew <amkoroew@users.noreply.github.com>
2024-11-15 21:53:45 +0100

    fix: null handling

--- Player/Program.cs
+++ Player/Program.cs
@@ -7,7 +7,7 @@ var app = WebApplication.Create(args);
 
 app.Urls.Add("http://*:3000");
 
-app.MapPost("/", (JsonElement gameState) => JsonConvert.SerializeObject(Strategy.Decide(JsonConvert.DeserializeObject<GameState>(gameState.GetRawText()))));
+app.MapPost("/", (JsonElement gameState) => JsonConvert.SerializeObject(Strategy.Decide(JsonConvert.DeserializeObject<GameState>(gameState.GetRawText()) ?? throw new InvalidOperationException())));
 
 app.MapGet("/", () => "\ud83d\udcff.bitbotΔƛΩπ");
 
