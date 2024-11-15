58a4671450e375fbe874f0fb7d40e00603f34b36
virlau <obihoernchen16@web.de>
2024-11-15 21:06:52 +0100

    add get highest and lowest level and find base by name

--- Logic/BaseUtils.cs
+++ Logic/BaseUtils.cs
@@ -14,6 +14,21 @@ public class BaseUtils
         return bases.OrderByDescending(i => i.Population).LastOrDefault();
     }
 
+    public static Base GetBaseWithHighestLevel(List<Base> bases)
+    {
+        return bases.OrderByDescending(i => i.Level).FirstOrDefault();
+    }
+
+    public static Base GetBaseWithLowestLevel(List<Base> bases)
+    {
+        return bases.OrderByDescending(i => i.Level).LastOrDefault();
+    }
+
+    public static Base GetBaseWithName(List<Base> bases, String name)
+    {
+        return bases.Find(i => name.Equals(name));
+    }
+
     public static Base[] GetMyBases(GameState gameState)
     {
         var myPlayerId = gameState.Game.Player;
