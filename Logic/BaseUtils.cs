ec680adc6894bf64555d211d838b07802420af4f
virlau <obihoernchen16@web.de>
2024-11-15 20:57:58 +0100

    add get base with lowest pops

--- Logic/BaseUtils.cs
+++ Logic/BaseUtils.cs
@@ -8,11 +8,15 @@ public class BaseUtils
     {
         return bases.OrderByDescending(i => i.Population).FirstOrDefault();
     }
-    
+
+    public static Base GetBaseWithLowestPops(List<Base> bases)
+    {
+        return bases.OrderByDescending(i => i.Population).LastOrDefault();
+    }
+
     public static Base[] GetMyBases(GameState gameState)
     {
         var myPlayerId = gameState.Game.Player;
         return gameState.Bases.Where(b => b.Player == myPlayerId).ToArray();
     }
-    
 }
