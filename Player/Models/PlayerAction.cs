4d136571118ca4f9bed275d9a9fe2900846cf09a
amkoroew <amkoroew@users.noreply.github.com>
2024-11-15 22:12:50 +0100

    refactor: avoid abbreviations

--- Player/Models/PlayerAction.cs
+++ Player/Models/PlayerAction.cs
@@ -5,10 +5,10 @@ namespace Player.Models;
 public class PlayerAction
 {
     [JsonProperty("src")]
-    public uint Src { get; set; }
+    public uint Source { get; set; }
 
     [JsonProperty("dest")]
-    public uint Dest { get; set; }
+    public uint Destination { get; set; }
 
     [JsonProperty("amount")]
     public uint Amount { get; set; }
