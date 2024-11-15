f5c46bd45f3fe9d805ebe75ac9d317c1b4b565ad
virlau <obihoernchen16@web.de>
2024-11-15 21:21:28 +0100

    add calc for remaining bits after traveling

--- Logic/Map.cs
+++ Logic/Map.cs
@@ -5,11 +5,18 @@ namespace PlayerDotNet.Logic;
 internal static class Map
 {
     internal static int CalculateDistanceBetweenBases(Base source, Base destination) =>
-        (int) Math.Sqrt(
+        (int)Math.Sqrt(
             Math.Pow(Math.Abs(source.Position.X - destination.Position.X), 2) +
             Math.Pow(Math.Abs(source.Position.Y - destination.Position.Y), 2) +
             Math.Pow(Math.Abs(source.Position.Z - destination.Position.Z), 2));
 
-    internal static List<Base> GetNeighbours(Base source, List<Base> bases) => 
+    internal static List<Base> GetNeighbours(Base source, List<Base> bases) =>
         bases.OrderBy(x => CalculateDistanceBetweenBases(source, x)).Skip(1).ToList();
+
+    public static uint GetRemainingBitsAfterTravel(Base source, Base destination, PathConfig pathConfig,
+        uint numberBits)
+    {
+        var remainingDistance = CalculateDistanceBetweenBases(source, destination) - pathConfig.GracePeriod;
+        return (uint)(remainingDistance <= 0 ? numberBits : numberBits - (remainingDistance * pathConfig.DeathRate));
+    }
 }
