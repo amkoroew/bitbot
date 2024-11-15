2bcb97cfe47e06261ecd324bf392f2b568b78b8d
virlau <obihoernchen16@web.de>
2024-11-15 20:51:34 +0100

    update get base with highest pops

--- Logic/BaseUtils.cs
+++ Logic/BaseUtils.cs
@@ -4,11 +4,8 @@ namespace PlayerDotNet.Logic;
 
 public class BaseUtils
 {
-    public static Base getBaseWithHighestPops(Base[] bases)
+    public static Base GetBaseWithHighestPops(List<Base> bases)
     {
-        return bases[0];
+        return bases.OrderByDescending(i => i.Population).FirstOrDefault();
     }
-    
-    
-    
 }
