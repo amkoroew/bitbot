5aef98add0a1b3373197e3446a27a0435447cef3
amkoroew <amkoroew@users.noreply.github.com>
2024-11-15 23:45:14 +0100

    feat: conquer next base

--- Player/Logic/Strategy.cs
+++ Player/Logic/Strategy.cs
@@ -4,8 +4,11 @@ namespace Player.Logic;
 
 public abstract class Strategy
 {
-    public static PlayerAction[] Decide(GameState gameState)
-    {
-        return [];
-    }
+    public static PlayerAction[] Decide(GameState gameState) =>
+        BaseUtils.GetMyBases(gameState).Select(x => new PlayerAction
+        {
+            Amount = x.Population,
+            Source = x.Uid,
+            Destination = Map.GetNeighbours(x, gameState.Bases).First().Uid
+        }).ToArray();
 }
