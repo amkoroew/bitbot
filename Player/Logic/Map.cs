2448ff4b8b37e8255c67d43582af079049b254ca
amkoroew <amkoroew@users.noreply.github.com>
2024-11-16 02:51:10 +0100

    feat: prioritize second base

--- Player/Logic/Map.cs
+++ Player/Logic/Map.cs
@@ -12,18 +12,15 @@ internal static class Map
 
     internal static List<Base> GetOwnBases(GameState gameState) =>
         gameState.Bases.Where(x => x.Player == gameState.Game.Player).ToList();
-
-    internal static List<Base> GetAbandonedBases(GameState gameState) =>
-        gameState.Bases.Where(x => x.Player == 0).ToList();
-
-    internal static List<Base> GetEnemyBases(GameState gameState) =>
-        gameState.Bases.Where(x => x.Player != 0 && x.Player != gameState.Game.Player).ToList();
-
-    internal static List<Base> GetNeighbours(GameState gameState, Base source) =>
-        gameState.Bases.OrderBy(x => CalculateDistanceBetweenBases(source, x)).Skip(1).ToList();
+    
+    internal static List<Base> GetForeignBases(GameState gameState) =>
+        gameState.Bases.Where(x => x.Player != gameState.Game.Player).ToList();
 
     internal static List<Base> GetOwnNeighbours(GameState gameState, Base source) =>
         GetOwnBases(gameState).OrderBy(x => CalculateDistanceBetweenBases(source, x)).Skip(1).ToList();
+
+    internal static List<Base> GetForeignNeighbours(GameState gameState, Base source) =>
+        GetForeignBases(gameState).OrderBy(x => CalculateDistanceBetweenBases(source, x)).Skip(1).ToList();
 
     internal static uint CalculateMoveCost(Base source, Base destination, PathConfig pathConfig) =>
         (uint)Math.Max(0,
@@ -31,4 +28,17 @@ internal static class Map
 
     internal static uint CalculateAmountAfterMove(Base source, Base destination, PathConfig pathConfig, uint amount) =>
         amount - CalculateMoveCost(source, destination, pathConfig);
+
+    public static List<(Base Destination, uint Cost)> CalculateConquerCosts(GameState gameState, Base source)
+    {
+        return GetForeignBases(gameState)
+            .Select(x => (Destination: x, Cost: CalculateConquerCost(gameState, source, x)))
+            .OrderBy(x => x.Cost)
+            .ToList();
+    }
+
+    private static uint CalculateConquerCost(GameState gameState, Base source, Base destination)
+    {
+        return CalculateMoveCost(source, destination, gameState.Config.Paths) + destination.Population + 1;
+    }
 }
--- Player/Logic/Strategy.cs
+++ Player/Logic/Strategy.cs
@@ -7,21 +7,67 @@ public abstract class Strategy
     public static PlayerAction[] Decide(GameState gameState)
     {
         var ownBases = Map.GetOwnBases(gameState);
-        if (ownBases.Count > 1)
+        if (ownBases.Count == 1)
         {
-            return ownBases.Select(x => new PlayerAction
+            var own = ownBases.Single();
+            var foreignNeighbours = Map.GetForeignNeighbours(gameState, own);
+            if (foreignNeighbours.Count != 0)
             {
-                Amount = BaseUtils.GetSpareBits(gameState, x),
-                Source = x.Uid,
-                Destination = Map.GetOwnNeighbours(gameState, x).Concat(Map.GetNeighbours(gameState, x)).First().Uid
-            }).ToArray();  
+                return
+                [
+                    new PlayerAction
+                    {
+                        Amount = own.Population - 1,
+                        Source = own.Uid,
+                        Destination = foreignNeighbours.First().Uid
+                    }
+                ];
+            }
+            if (BaseUtils.GetUpgradeCost(gameState, own) < own.Population)
+            {
+                return
+                [
+                    new PlayerAction
+                    {
+                        Amount = BaseUtils.GetUpgradeCost(gameState, own),
+                        Source = own.Uid,
+                        Destination = own.Uid
+                    }
+                ];
+            }
         }
-        
-        return ownBases.Where(x => BaseUtils.GetUpgradeCost(gameState, x) < x.Population).Select(x => new PlayerAction
+
+        return ownBases.Select(x =>
         {
-            Amount = BaseUtils.GetUpgradeCost(gameState, x),
-            Source = x.Uid,
-            Destination = x.Uid
+            var spareBits = BaseUtils.GetSpareBits(gameState, x);
+            
+            if (spareBits > BaseUtils.GetUpgradeCost(gameState, x))
+            {
+                return new PlayerAction
+                {
+                    Amount = BaseUtils.GetUpgradeCost(gameState, x),
+                    Source = x.Uid,
+                    Destination = x.Uid
+                };
+            }
+
+            var minimalConquerCost = Map.CalculateConquerCosts(gameState, x).First().Cost;
+            if (minimalConquerCost < spareBits)
+            {
+                return new PlayerAction
+                {
+                    Amount = minimalConquerCost,
+                    Source = x.Uid,
+                    Destination = x.Uid
+                };
+            }
+            
+            return new PlayerAction
+            {
+                Amount = spareBits,
+                Source = x.Uid,
+                Destination = Map.GetOwnNeighbours(gameState, x).First().Uid
+            };
         }).ToArray();
     }
 }
