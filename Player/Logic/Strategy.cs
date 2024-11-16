be21b282189dd54e597f0dfd8e8f6ad90c60c757
amkoroew <amkoroew@users.noreply.github.com>
2024-11-16 01:15:43 +0100

    feat: log actions

--- Player/Logic/Strategy.cs
+++ Player/Logic/Strategy.cs
@@ -1,21 +1,14 @@
-﻿using Newtonsoft.Json;
-using Player.Models;
+﻿using Player.Models;
 
 namespace Player.Logic;
 
 public abstract class Strategy
 {
-    public static PlayerAction[] Decide(GameState gameState)
-    {
-        var actions = BaseUtils.GetMyBases(gameState).Select(x => new PlayerAction
+    public static PlayerAction[] Decide(GameState gameState) =>
+        BaseUtils.GetMyBases(gameState).Select(x => new PlayerAction
         {
             Amount = x.Population,
             Source = x.Uid,
             Destination = Map.GetOwnNeighbours(gameState, x).Concat(Map.GetNeighbours(gameState, x)).First().Uid
         }).ToArray();
-        
-        Console.Write(JsonConvert.SerializeObject(actions));
-        
-        return actions;
-    }
 }
--- Player/Program.cs
+++ Player/Program.cs
@@ -1,13 +1,23 @@
 using System.Text.Json;
-using Newtonsoft.Json;
 using Player.Logic;
 using Player.Models;
+using static Newtonsoft.Json.JsonConvert;
 
 var app = WebApplication.Create(args);
 
+var logger = LoggerFactory.Create(config => { config.AddConsole(); }).CreateLogger("Program");
+
 app.Urls.Add("http://*:3000");
 
-app.MapPost("/", (JsonElement gameState) => Strategy.Decide(JsonConvert.DeserializeObject<GameState>(gameState.GetRawText()) ?? throw new InvalidOperationException()));
+app.MapPost("/", (JsonElement gameState) =>
+{
+    logger.LogInformation("=== thinking started ===");
+    var actions = Strategy.Decide(DeserializeObject<GameState>(gameState.GetRawText()) ??
+                                  throw new InvalidOperationException());
+    logger.LogInformation(SerializeObject(actions));
+    logger.LogInformation("=== thinking completed ===");
+    return actions;
+});
 
 app.MapGet("/", () => "\ud83d\udcff.bitbotΔƛΩπ");
 
